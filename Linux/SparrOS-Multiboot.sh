370d1d297fe63867137ff5334c0b875312bdb870
SparrOSDeveloperTeam <joseph.hackett@madrid.k12.ia.us>
2017-02-24 11:21:33 -0600

    Update SparrOS-Multiboot.sh

--- Linux/SparrOS-Multiboot.sh
+++ Linux/SparrOS-Multiboot.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+#
+# Makes directory /SparrOS/ISO/Output as root
 
+sudo mkdir /SparrOS/ISO
+sudo mkdir -p /SparrOS/ISO/Output
+cd /SparrOS/ISO
+
+# Mounts the ISO from ./
+
+mkdir /mnt/tmp

6bb7fa037ad69932bfdd8d71e4422d3e540723c8
SparrOSDeveloperTeam <joseph.hackett@madrid.k12.ia.us>
2017-02-24 12:01:52 -0600

    Update SparrOS-Multiboot.sh

--- Linux/SparrOS-Multiboot.sh
+++ Linux/SparrOS-Multiboot.sh
@@ -28,3 +28,14 @@ cp /mnt/tmpx/initrd.gz /mnt/tmpx/vmlinuz  /SparrOS/ISO/Output/boot/os
 mkdir /SparrOS/ISO/Output/boot/grub
 
 # If you have grub on your system,
+
+rpm grub rpm -ql grub | grep stage2_eltorito
+locate stage2_eltorito
+cp /path_to/eltorito_dir/stage2_eltorito  /SparrOS/ISO/Output/boot/grub/
+
+# Syslinux needs to be installed
+
+locate vesamenu.c32 isolinux.bin
+rpm -ql syslinux | grep vesamenu.c32 &&  rpm -ql syslinux | grep isolinux.bin
+mkdir mbcd/boot/isolinux
+cp /path_to/vesamenu.c32 /path_to/isolinux.bin  /SparrOS/ISO/Output/boot/isolinux
